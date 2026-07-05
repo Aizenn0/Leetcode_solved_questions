5b0b3a6fb987ac7ee969095973b2d236c99e88e2
Sanchit Shuvam Behera <sanchitshuvam2006@gmail.com>
2026-07-06 02:16:29 +0530

    Time: 0 ms (100%), Space: 8 MB (86.71%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int c2=0,c5=0;
-        for(int i=1; i<=n; i++){
-            int x=i;
-            while(x%5==0){
-                x/=5;
-                c5++;
-            }
+        int ans=0;
+        while(n>=5){
+            ans+=n/5;
+            n/=5;
         }
-        return c5;
+        return ans;
     }
 };
