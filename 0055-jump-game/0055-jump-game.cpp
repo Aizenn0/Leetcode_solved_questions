06b7ce1124c8c598d0ad748e16bf2920138ef70a
Sanchit Shuvam Behera <sanchitshuvam2006@gmail.com>
2026-07-05 12:07:28 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.63%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n= nums.size();
+        int t=n-1;
+        for(int i=n-2; i>=0; i--){
+            if(i+nums[i]>=t)t=i;
+        }
+        return t==0;
+    }
+};
